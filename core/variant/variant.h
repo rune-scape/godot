f89a4129ab7af93c1215d4899d3ba53fa28b7ba9
rune-scape <allie.smith.epic@gmail.com>
2023-04-20 01:43:15 -0700

    Added operator overloading

--- core/variant/variant.h
+++ core/variant/variant.h
@@ -540,6 +540,10 @@ public:
 	};
 
 	static String get_operator_name(Operator p_op);
+	static StringName get_op_overload_name(Operator p_op);
+	static StringName get_op_roverload_name(Operator p_op);
+	static bool is_op_unary(Operator p_op);
+	static bool is_op_boolean(Operator p_op);
 	static void evaluate(const Operator &p_op, const Variant &p_a, const Variant &p_b, Variant &r_ret, bool &r_valid);
 	static _FORCE_INLINE_ Variant evaluate(const Operator &p_op, const Variant &p_a, const Variant &p_b) {
 		bool valid = true;
