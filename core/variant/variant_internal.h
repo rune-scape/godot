d1720c690df095188d7dd4a8fca210271769ddae
rune-scape <allie.smith.epic@gmail.com>
2024-01-25 22:45:40 -0800

    GDExtension method calls should not pass nullptr args

--- core/variant/variant_internal.h
+++ core/variant/variant_internal.h
@@ -356,10 +356,11 @@ public:
 		}
 	}
 
+	inline static void *static_nil_ptr = nullptr;
 	_FORCE_INLINE_ static void *get_opaque_pointer(Variant *v) {
 		switch (v->type) {
 			case Variant::NIL:
-				return nullptr;
+				return &static_nil_ptr;
 			case Variant::BOOL:
 				return get_bool(v);
 			case Variant::INT:
@@ -445,7 +446,7 @@ public:
 	_FORCE_INLINE_ static const void *get_opaque_pointer(const Variant *v) {
 		switch (v->type) {
 			case Variant::NIL:
-				return nullptr;
+				return &static_nil_ptr;
 			case Variant::BOOL:
 				return get_bool(v);
 			case Variant::INT:
