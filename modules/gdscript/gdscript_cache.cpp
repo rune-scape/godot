ebf9eaab55601cae90c044b6c272a4d215d3e9d7
rune-scape <spartacrafter@gmail.com>
2024-08-09 15:28:43 -0700

    GDScriptCache: Invalidate moved script

--- modules/gdscript/gdscript_cache.cpp
+++ modules/gdscript/gdscript_cache.cpp
@@ -155,10 +155,7 @@ void GDScriptCache::move_script(const String &p_from, const String &p_to) {
 		return;
 	}
 
-	if (singleton->parser_map.has(p_from) && !p_from.is_empty()) {
-		singleton->parser_map[p_to] = singleton->parser_map[p_from];
-	}
-	singleton->parser_map.erase(p_from);
+	remove_parser(p_from);
 
 	if (singleton->parser_inverse_dependencies.has(p_from) && !p_from.is_empty()) {
 		singleton->parser_inverse_dependencies[p_to] = singleton->parser_inverse_dependencies[p_from];
